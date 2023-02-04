5b4e106eaa04ab923c9e0ac4f7418600cde4e18c
sivanov <sivanov@avancore.ru>
2023-02-04 21:15:07 +0300

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env.deploy') });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_REF, DEPLOY_REPO, DEPLOY_PATH,
+} = process.env;
+
+module.exports = {
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'post-deploy': 'docker-compose up --build',
+    },
+  },
+};
