028e5121aaec9fc421ef9c064595143672661ad5
sivanov <sivanov@avancore.ru>
2023-02-04 21:18:04 +0300

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,5 +1,5 @@
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '.env.deploy') });
+
+require('dotenv').config({ path: '.env.deploy' });
 
 const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_REF, DEPLOY_REPO, DEPLOY_PATH,
