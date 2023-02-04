bca88274e03809948b2e73fd0075035a20f047b9
sivanov <sivanov@avancore.ru>
2023-02-04 23:07:26 +0300

    deploy front

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = 'http://host.docker.internal:4000' ; //"http://127.0.0.1:4000";
+export const URL = 'http://api.termit.nomoredomainsclub.ru' ; //"http://127.0.0.1:4000";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
